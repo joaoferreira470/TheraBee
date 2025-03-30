a84347e0d98c841825e7acad0265ce93c2a75a91
João Ferreira <jmp.ferreira92@gmail.com>
2025-03-30 22:54:39 +0100

    Estrutura Domain

--- src/Services/Patients/Patients.Domain/Models/Patient.cs
+++ src/Services/Patients/Patients.Domain/Models/Patient.cs
@@ -1,5 +1,16 @@
-﻿namespace Patients.Domain.Models;
+﻿using Patients.Domain.Abstractions;
+using Patients.Domain.ValueObjects;
 
-public class Patient //Aqui herdaria das abstractions
+namespace Patients.Domain.Models;
+
+public class Patient : Aggregate<Guid>
 {
+    public string Name { get; set; } = default!;
+    DateTime DateOfBirth { get; set; } = default!;
+    public Address PatientAddress { get; set; } = default!;
+
+    public string Diagnosis {  get; set; } = default!;
+    public string Info { get; set;} = default!;
+    public Guid TherapistId { get; set; } = default!;
+
 }
---
+++ src/Services/Patients/Patients.Domain/ValueObjects/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Patients.Domain.ValueObjects;
+
+public record Address
+{
+    public string AddressLine { get; set; } = default!;
+    public string District { get; set; } = default!;
+    public string Location { get; set; } = default!;
+    public string ZipCode { get; set; } = default!;
+}
