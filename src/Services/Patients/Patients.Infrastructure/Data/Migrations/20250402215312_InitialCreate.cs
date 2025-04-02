19c0765ab249d0b26cb6c48c6e107b52985b9e21
João Ferreira <jmp.ferreira92@gmail.com>
2025-04-02 23:43:38 +0100

    Preparação Pré Containerização

---
+++ src/Services/Patients/Patients.Infrastructure/Data/Migrations/20250402215312_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Patients.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Diagnosis = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    Info = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    TherapistId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PatientAddress_AddressLine = table.Column<string>(type: "character varying(180)", maxLength: 180, nullable: false),
+                    PatientAddress_District = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    PatientAddress_Location = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    PatientAddress_ZipCode = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
