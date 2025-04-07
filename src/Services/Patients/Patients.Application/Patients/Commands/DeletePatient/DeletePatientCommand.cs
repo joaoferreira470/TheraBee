d8084d3c1cbb366dd1485018346914bd83ac21fd
João Ferreira <jmp.ferreira92@gmail.com>
2025-04-07 20:35:44 +0100

    Criação da operação Delete

---
+++ src/Services/Patients/Patients.Application/Patients/Commands/DeletePatient/DeletePatientCommand.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Patients.Application.Patients.Commands.UpdatePatient;
+
+namespace Patients.Application.Patients.Commands.DeletePatient;
+
+public record DeletePatientCommand(Guid PatientId)
+    : ICommand<DeletePatientResult>;
+
+public record DeletePatientResult(bool IsSuccess);
+
+public class DeletePatientCommandValidator : AbstractValidator<DeletePatientCommand>
+{
+    public DeletePatientCommandValidator()
+    {
+        RuleFor(x => x.PatientId).NotEmpty().WithMessage("Id is required");
+    }
+}
---
+++ src/Services/Patients/Patients.Application/Patients/Commands/DeletePatient/DeletePatientHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Patients.Application.Patients.Commands.DeletePatient;
+
+public class DeletePatientHandler(IApplicationDbContext dbContext) : ICommandHandler<DeletePatientCommand, DeletePatientResult>
+{
+    public async Task<DeletePatientResult> Handle(DeletePatientCommand command, CancellationToken cancellationToken)
+    {
+        var patientId = PatientId.Of(command.PatientId);
+
+        var patient = await dbContext.Patients.FindAsync([patientId], cancellationToken);
+
+        if (patient == null)
+        {
+            throw new PatientNotFoundException(command.PatientId);
+        }
+
+        dbContext.Patients.Remove(patient);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeletePatientResult(true);
+    }
+}
