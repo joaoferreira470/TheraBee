b1742a9615964eca91fe5d999a49d04319166074
João Ferreira <jmp.ferreira92@gmail.com>
2025-04-08 01:26:32 +0100

    Introdução de PipelineBehaviors

--- src/Services/Patients/Patients.Application/DependencyInjection.cs
+++ src/Services/Patients/Patients.Application/DependencyInjection.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Patients.Application;
 public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
         return services;
     }
 }
